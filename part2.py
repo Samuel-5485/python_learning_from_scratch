5e5606b11b6eb3978f002b80cfef469dd7c8948f
Sami Diriba <samediriba54@gmail.com>
2026-01-31 16:16:13 +0300

    this's the second file, that talk about user input and solve problem related to it

---
+++ part2.py
@@ -0,0 +1,8 @@
+# User input
+user_input = input("Enter something:")
+print("You entered:", user_input)
+
+#a program that accept side of a square and display the area
+side = float(input("Enter the side length of the square:"))
+area = side*side
+print("The area of the square is:", area)
