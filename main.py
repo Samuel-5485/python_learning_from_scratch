6cf9b88fc26484e6dd83cbc2d6dab8c1fd65a59c
Sami Diriba <samediriba54@gmail.com>
2026-01-31 15:40:02 +0300

    hey, i've started learning python journey for machine learning

---
+++ main.py
@@ -0,0 +1,11 @@
+print("Hey bro welcome to python")
+print("Hello","world") 
+print("python "*2)
+print("python\n"*2)
+print("python "+"python")
+name = "Sami"
+age = 22
+print("My name is", name, "and I am", age,"years old.")
+
+# Variables and Data Types
+_first_name = "John"
