2da424532ebf53b39b9d0085002801a5b24c6445
Sami Diriba <samediriba54@gmail.com>
2026-01-31 16:00:20 +0300

    I've finished the variables, datatype

--- main.py
+++ main.py
@@ -9,3 +9,21 @@ print("My name is", name, "and I am", age,"years old.")
 
 # Variables and Data Types
 _first_name = "John"
+x = 4
+y = x
+x1 = 3.6
+z = True
+print(y)
+
+print(type(_first_name))
+print(type(x))
+print(type(x1))
+print(type(z))
+x = str(x)
+print(type(x))
+
+pi = 3.14
+radius = 4
+area = pi *radius*radius
+print("Area of circle is:", area)
+
