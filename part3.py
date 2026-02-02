f34e39d3a986d1070ae95efae60b783fa5f71ec7
Sami Diriba <samediriba54@gmail.com>
2026-02-02 13:20:02 +0300

    I have covered this part

--- part3.py
+++ part3.py
@@ -50,7 +50,50 @@ if num%2 ==0:
 else:
     print("The number is odd")
 
+# Write a program to determine if a student is eligible to vote based on their age.
+age = int(input("Enter your age:"))
+if age>=18:
+    print("You're eligible to vote")
+else:
+    print("You're not eligible to vote yet")
 
+# Nested if statements
+a = 89
+b = 67
+c = 90
 
+if a>b:
+    if a>c:
+        print("a is the largest number")
+    else:
+        print("c is the largest number")
+elif b>c:
+    print("b is the largest number")
+else:
+    print("c is the largest number")
 
+# Write a program that accept student score and display the grade
+score = float(input("Hye, what is your score?"))
+if score >=90:
+    print("Your grade is A+")
+elif score >=80:
+    print("Your grade is A")
+elif score >=70:
+    print("Your grade is B")
+elif score >=60:
+    print("Your grade is C")
+elif score >=50:
+    print("Your grade is D")
+else:
+    print("Your grade is F, \n you need to take the course again")
 
+# if-elif-else
+# Write a python program to determine the largest of the three numbers
+
+# A shop will give discount of 10% if the cost of purchased quantity is more than 1000
+
+total_item = int(input("Enter how many items did you purchase: "))
+initial_cost = total_item*100
+if initial_cost > 1000:
+    initial_cost = initial_cost - (initial_cost * 0.1)
+print("The total cost after discount is:", initial_cost)
