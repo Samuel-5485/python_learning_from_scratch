da17cc2285b4b07a836dc933ecc0f704221b9cf9
Sami Diriba <samediriba54@gmail.com>
2026-02-03 10:53:08 +0300

    just now i have started for loop

--- loop.py
+++ loop.py
@@ -1,7 +1,13 @@
 # Loop in python
-# while
+# While loop
 i = 3
 while i != 0:
     print("Python")
     i -= 1 #decrement
 
+i = 1
+while i <= 4:
+    print("Learning together!")
+    i += 1
+
+# For loop
