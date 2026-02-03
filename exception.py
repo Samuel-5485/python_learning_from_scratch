9671b0afa8257c38415b4c98b3aca7a3d9b46481
Sami Diriba <samediriba54@gmail.com>
2026-02-03 22:10:49 +0300

    I have learned exception handling using functions, try, except, pass, break

--- exception.py
+++ exception.py
@@ -8,11 +8,32 @@ else:
     print(f"x is {x}")
 # print(f"x is {x}") #NameError: name 'x' is not defined
 
-while True:
-    try:
-        y = int(input("What's y? "))
-    except ValueError:
-        print("y is not integer")
-    else:
-        break
-print(f"y is {y}")
+#  Exception handling: function, break
+def main():
+    y = get_int()
+    print(f"y is {y}")
+
+def get_int():
+    while True:
+        try:
+            y = int(input("What's y? "))
+        except ValueError:
+            print("y is not integer")
+        else:
+            break
+    return y
+main()
+
+# Exception handling: function, pass
+def main():
+    z = get_int()
+    print(f"z is {z}")
+
+def get_int():
+    while True:
+        try:
+            return int(input("What's z? "))
+        except ValueError:
+            pass
+        
+main()
