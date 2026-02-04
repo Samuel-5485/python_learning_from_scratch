21d8213baf4d84b516ac9427c84ee8093692f8aa
Sami Diriba <samediriba54@gmail.com>
2026-02-04 18:37:35 +0300

    imported sys

--- libraries.py
+++ libraries.py
@@ -1,5 +1,6 @@
 import random 
 import statistics
+import sys
 """
 coin = random.choice(["heads", "tails"])
 print(coin)
@@ -16,4 +17,17 @@ for card in cards:
     print(card)
 """
 #take a look docs.python.org/3/library/statistics.html
-print(statistics.mean([40, 30]))
+# print(statistics.mean([40, 30]))
+"""
+try:
+    print("hey, welcome to learn with me!", sys.argv[1])
+except IndexError:
+    print("Too few arguments")
+"""
+if len(sys.argv) < 2:
+    print("Too few arguments")
+elif len(sys.argv) > 2:
+    print("Too many arguments")
+else:
+    print("hello, my name is", sys.argv[1])
+
