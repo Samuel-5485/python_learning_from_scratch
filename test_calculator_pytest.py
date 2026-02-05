759523e8fd9c3a9dd7285db7ee3bf9ebe1eae028
Sami Diriba <samediriba54@gmail.com>
2026-02-05 09:37:53 +0300

    I have imported pytest library to perform with pytest.raises...

--- test_calculator_pytest.py
+++ test_calculator_pytest.py
@@ -1,7 +1,15 @@
 from unit_test import square
+import pytest  #imported pytest library
 #this program check for pytest run python -m pytest
-def test_square():
+def test_positive():
     assert square(2) == 4
-    assert square(3) == 9 
+    assert square(3) == 9
+def test_negative(): 
     assert square(-3) == 9
     assert square(-2) == 4
+def test_zero():
+    assert square(0) == 0
+
+def test_str():
+    with pytest.raises(TypeError):
+        square("cow")
