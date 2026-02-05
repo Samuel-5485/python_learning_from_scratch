7c55a41c37b6ba6a658a2ea608e5136a60d5681d
Sami Diriba <samediriba54@gmail.com>
2026-02-05 08:06:00 +0300

    I have started new topic unit test

---
+++ unit_test.py
@@ -0,0 +1,9 @@
+def main():
+    y = int(input("What's y? "))
+    print("ÿ squared is", square(y))
+
+def square(n):
+    return n+n
+if __name__ == "__main__":
+    main() #what'll happen ifn't close by this
+    
